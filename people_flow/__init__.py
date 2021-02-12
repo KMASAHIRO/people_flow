f99975d068bff3a4e7a0598fb0f809978631ad14
KMASAHIRO <k.masa1215@gmail.com>
2021-02-13 01:04:52 +0900

    fifth commit

--- people_flow/__init__.py
+++ people_flow/__init__.py
@@ -1,3 +1,6 @@
 from . import simulation
 from . import assessment_framework
 from . import inference_framework
+from . import simulation_colab
+from . import assessment_framework_colab
+from . import inference_framework_colab
