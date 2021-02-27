fe40b43b0b6690d109096f704ad788bc92434be0
KMASAHIRO <k.masa1215@gmail.com>
2021-02-27 09:51:53 +0900

    sixth commit

--- people_flow/__init__.py
+++ people_flow/__init__.py
@@ -4,3 +4,9 @@ from . import inference_framework
 from . import simulation_colab
 from . import assessment_framework_colab
 from . import inference_framework_colab
+
+import subprocess
+
+subprocess.call(['pip','install','numpy==1.19.2'])
+subprocess.call(['pip','install','matplotlib==3.3.2'])
+subprocess.call(['pip','install','optuna==2.4.0'])
